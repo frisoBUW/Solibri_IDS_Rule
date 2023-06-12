a270f667d5693ce0a18aa353fce436e6de2a1172
FriSch <96592011+frisoBUW@users.noreply.github.com>
2023-06-12 21:49:42 +0200

    Was half working, constantly throws error because One by One Rule seems to even run preCheck() concurrently. After this I will try to just implement a basic Rule and hope this will do.

--- src/main/java/com/solibri/rule/IdsParser.java
+++ src/main/java/com/solibri/rule/IdsParser.java
@@ -9,13 +9,9 @@ import javax.xml.bind.Unmarshaller;
 import java.io.File;
 
 public class IdsParser {
-    private String filename;
 
-    public IdsParser(String filename) {
-        this.filename = filename;
-    }
-
-    public Ids parseIdsFile() {
+    public Ids parseIdsFile(String filename) {
+        Ids ids = null;
         try {
             // Disable JAXB optimizations to fix error with JDK 9+ versions
             System.setProperty("com.sun.xml.bind.v2.bytecode.ClassTailor.noOptimize", "true");
@@ -27,51 +23,52 @@ public class IdsParser {
             Unmarshaller unmarshaller = jaxbContext.createUnmarshaller();
 
             // Use the Unmarshaller to unmarshal the IDS XML file
-            Ids ids = (Ids) unmarshaller.unmarshal(new File(filename));
+            ids = (Ids) unmarshaller.unmarshal(new File(filename));
 
         } catch (JAXBException e) {
             e.printStackTrace();
         }
-        return null;
+        return ids;
     }
 
-    private void setUp(Ids ids) {
+//    private void setUp(Ids ids) {
+//
+//        // Extract and use the Info and Specifications
+//        Ids.Info info = ids.getInfo();
+//        Ids.Specifications specifications = ids.getSpecifications();
+//
 
-        // Extract and use the Info and Specifications
-        Ids.Info info = ids.getInfo();
-        Ids.Specifications specifications = ids.getSpecifications();
+//        // You can now use the info and specifications objects in your application
+//        printInfoTitle(info);
+//        printSpecificationsClass(specifications);
+//        printSpecificationDetails(ids);
+//    }
 
-        // You can now use the info and specifications objects in your application
-        printInfoTitle(info);
-        printSpecificationsClass(specifications);
-        printSpecificationDetails(ids);
-    }
-
-    private void printInfoTitle(Ids.Info info) {
-        System.out.println("Title: " + info.getTitle());
-    }
-
-    private void printSpecificationsClass(Ids.Specifications specifications) {
-        System.out.println("Specifications class: " + specifications.getClass().getName());
-    }
-
-    private void printSpecificationDetails(Ids ids) {
-        for (SpecificationType specification : ids.getSpecifications().getSpecification()) {
-            String name = specification.getName();
-            String entity = specification.getApplicability().getEntity().getName().getSimpleValue();
-            System.out.println("Specification: " + name + " -- Entity: " + entity);
-        }
-    }
-
-    public static void main(String[] args) {
-        String filename = "C:\\Projects\\_Java\\Solibri\\view-examples\\src\\main\\resources\\IDS_Test_5D.xml";
-
-        IdsParser idsParser = new IdsParser(filename);
-        Ids ids = idsParser.parseIdsFile();
-
-        // Extract and use the Info and Specifications
-        Ids.Info info = ids.getInfo();
-        Ids.Specifications specifications = ids.getSpecifications();
-
-    }
+//    private void printInfoTitle(Ids.Info info) {
+//        System.out.println("Title: " + info.getTitle());
+//    }
+//
+//    private void printSpecificationsClass(Ids.Specifications specifications) {
+//        System.out.println("Specifications class: " + specifications.getClass().getName());
+//    }
+//
+//    private void printSpecificationDetails(Ids ids) {
+//        for (SpecificationType specification : ids.getSpecifications().getSpecification()) {
+//            String name = specification.getName();
+//            String entity = specification.getApplicability().getEntity().getName().getSimpleValue();
+//            System.out.println("Specification: " + name + " -- Entity: " + entity);
+//        }
+//    }
+//
+//    public static void main(String[] args) {
+//        String filename = "C:\\Projects\\_Java\\Solibri\\view-examples\\src\\main\\resources\\IDS_Test_5D.xml";
+//
+//        IdsParser idsParser = new IdsParser(filename);
+//        Ids ids = idsParser.parseIdsFile();
+//
+//        // Extract and use the Info and Specifications
+//        Ids.Info info = ids.getInfo();
+//        Ids.Specifications specifications = ids.getSpecifications();
+//
+//    }
 }
--- src/main/java/com/solibri/rule/IdsRule.java
+++ src/main/java/com/solibri/rule/IdsRule.java
@@ -1,46 +1,90 @@
 package com.solibri.rule;
 
 import com.solibri.rule.IdsParser;
+import com.solibri.smc.api.SMC;
 import com.solibri.smc.api.checking.*;
 import com.solibri.smc.api.filter.ComponentFilter;
+import com.solibri.smc.api.ifc.IfcEntityType;
 import com.solibri.smc.api.model.Component;
+import de.buildingsmart.ids.Ids;
+import de.buildingsmart.ids.SpecificationType;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 
 import java.lang.invoke.MethodHandles;
 import java.util.*;
 
+import static com.solibri.smc.api.ifc.IfcEntityType.*;
+
 public final class IdsRule extends OneByOneRule {
 
-	private static final Logger LOG = LoggerFactory.getLogger(MethodHandles.lookup().lookupClass());
-
-
-	private final RuleParameters params = RuleParameters.of(this);
+	public Ids ids = new IdsParser().parseIdsFile("C:\\Projects\\_Python\\SA\\Data\\IDS\\IDS_Test_5D.xml");
 
 	public final FilterParameter rpComponentFilter = this.getDefaultFilterParameter();
 
 	private static final String COMPONENT_FILTER_PARAMETER_ID2 = "rpComponentFilter2";
 
+	private final RuleParameters params = RuleParameters.of(this);
+
 	public final FilterParameter rpComponentFilter2 = params.createFilter(COMPONENT_FILTER_PARAMETER_ID2);
 
 	//    Actually ids stuff
-	private final StringParameter filename = params.createString("MyStringParameter");
+	public final StringParameter filename = params.createString("MyStringParameter");
 
+	private static final Logger LOG = LoggerFactory.getLogger(MethodHandles.lookup().lookupClass());
+
+	private final RuleResources resources = RuleResources.of(this);
+
+	public void setIds(Ids ids) {
+		this.ids = ids;
+	}
+
+	@Override
+	public PreCheckResult preCheck() {
+		String stringParameterValue = filename.getValue();
+		boolean isRelevant = isFileExtensionValid(stringParameterValue);
+		if (isRelevant) {
+			LOG.info("Ids Contents: " + ids.getInfo().getTitle());
+			for (SpecificationType specification : ids.getSpecifications().getSpecification()) {
+				LOG.info("Specification: " + specification.getName() + " -- Entity: " + specification.getApplicability().getEntity().getName().getSimpleValue());
+			}
+		}
+		return isRelevant ? PreCheckResult.createRelevant() : PreCheckResult.createIrrelevant();
+	}
+
+	private boolean isFileExtensionValid(String filePath) {
+		String lowerCaseFilePath = filePath.toLowerCase();
+		return lowerCaseFilePath.endsWith(".xml") || lowerCaseFilePath.endsWith(".ids");
+	}
 
 	@Override
 	public Collection<Result> check(Component component, ResultFactory resultFactory) {
+
 		String stringParameterValue = filename.getValue();
 
-		ComponentFilter requirementFilter = rpComponentFilter2.getValue();
-		LOG.info("Ids Contents: {]" + requirementFilter);
-		
 
-		IdsParser idsParser = new IdsParser(stringParameterValue);
-		LOG.info("Ids Contents: {]" + idsParser);
+		Collection<Component> applicableElements = SMC.getModel().getComponents(rpComponentFilter.getValue());
+
 
 		ResultCategory resultCategory = resultFactory.createCategory("TestUniqueName", "TestDisplayName");
-
 		Result result = resultFactory.create(stringParameterValue, "DescriptionTrue", resultCategory);
-		return Collections.singleton(result);
+
+		Collection<Result> results = new ArrayList<>();
+
+		for (Component element : applicableElements) {
+			IfcEntityType entity = element.getIfcEntityType().get();
+			if (entity == IfcRoof) {
+				results.add(result);
+			} else break;
+		}
+
+
+		return results;
+	}
+
+	Result createResult(ResultFactory resultFactory, IfcEntityType entity) {
+		return resultFactory.create(resources.getString("Result.TestFactory.Name"),
+				resources.getString("Result.TestFactory.Description",
+						entity.toString()));
 	}
 }
